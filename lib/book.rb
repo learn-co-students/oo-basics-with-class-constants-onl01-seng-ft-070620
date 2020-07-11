4114fe11735644547edf523b67faa75fd40d2457
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-10 18:27:00 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,28 @@
+## 1. Create a class constant GENRES that
+##    tracks all of the genres of all the
+##    books we create.
+## 2. Add a #genre= method
+## 3. Remove #genre from attr_accessor and add
+##    #genre to attr reader 
+## 4. Add to GENRES constant when #genre's
+##    are created
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
   end
 
+  def genre=(genre)
+    @genre = genre 
+    GENRES << genre 
+  end 
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,25 @@
+## 1. Create a class constant BRANDS that
+##    tracks all of the genres of all the
+##    books we create.
+## 2. Add to BRANDS constant when #brands
+##    are created, only if they are unique
+##    brands.
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end 
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
